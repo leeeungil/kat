811268466c2bb4103a2fe7ecd02c3cddc9a8592c
DESKTOP-A9SUE12\tutu1 <wnsdud159!>
2018-03-27 00:48:10 +0900

    j
    j

--- src/main/resources/sql_bak/create_table.sql
+++ src/main/resources/sql_bak/create_table.sql
@@ -1,80 +1,133 @@
-DROP TABLE BOARD;
-CREATE TABLE BOARD (
-  `board_no` int(11) NOT NULL,
-  `board_title` varchar(200) DEFAULT NULL,
-  `board_content` varchar(2000) DEFAULT NULL,
-  `regdate` datetime DEFAULT NULL,
-  `user_id` varchar(200) DEFAULT NULL,
-  `writer_name` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `board_reply` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`board_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE FILE;
-CREATE TABLE FILE (
-  `file_no` int(11) NOT NULL AUTO_INCREMENT,
-  `file_seat` int(11) NOT NULL,
-  `file_seatphoto` varchar(200) NOT NULL,
-  `create_date` datetime NOT NULL,
-  `user_id` varchar(50) NOT NULL,
-  `seat_status` varchar(50) NOT NULL,
-  `reservation_id` varchar(50) NOT NULL,
-  `delete_seat` varchar(50) NOT NULL,
-  `recenttime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`file_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 DROP TABLE MASTER;
 CREATE TABLE MASTER (
   `master_no` int(11) NOT NULL AUTO_INCREMENT,
   `master_id` varchar(45) NOT NULL,
   `master_pw` varchar(45) NOT NULL,
-  `regdate` datetime NOT NULL,
-  `user_id` varchar(200) NOT NULL,
+  `create_date` datetime NOT NULL,
   PRIMARY KEY (`master_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/* 문의하기 테이블 */
+DROP TABLE ASK_TO;
+CREATE TABLE ASK_TO (
+  `ask_no` int(11) NOT NULL AUTO_INCREMENT,
+  `ask_title` varchar(200) DEFAULT NULL,
+  `ask_content` varchar(2000) DEFAULT NULL,
+  `user_id` varchar(200) DEFAULT NULL,
+  `writer_name` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `ask_reply` varchar(300) DEFAULT NULL,
+  `create_date` datetime NOT NULL,
+  `DELETE_FLAG` INT(2),
+  PRIMARY KEY (`ask_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* 예약하기 테이블 */
+DROP TABLE RESERVATION;
+CREATE TABLE RESERVATION (
+  `reserv_no` int(11) NOT NULL AUTO_INCREMENT,
+  `reserv_id` varchar(100) NOT NULL,
+  `people_number` varchar(50) NOT NULL,
+  `reserv_time` datetime NOT NULL,
+  `reserv_phone` varchar(45) NOT NULL,
+  `DELETE_FLAG` INT(2),
+  PRIMARY KEY (`reserv_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* 홈페이지 공지사항 테이블 */
 DROP TABLE NOTICE;
 CREATE TABLE NOTICE (
   `notice_no` int(11) NOT NULL AUTO_INCREMENT,
   `notice_title` varchar(200) NOT NULL,
   `notice_content` varchar(2000) NOT NULL,
-  `user_id` varchar(50) NOT NULL,
-  `regdate` datetime NOT NULL,
+  `master_no` varchar(50) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `DELETE_FLAG` INT(2),
   PRIMARY KEY (`notice_no`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-DROP TABLE RESERVATION;
-CREATE TABLE RESERVATION (
-  `file_no` int(11) NOT NULL AUTO_INCREMENT,
-  `reservation_id` varchar(100) NOT NULL,
-  `reservation_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `reservation_phone` varchar(45) NOT NULL,
-  PRIMARY KEY (`file_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE SHOP_PHOTO;
-CREATE TABLE SHOP_PHOTO (
-  `photo_no` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(50) NOT NULL,
-  `file_shopphoto` varchar(200) NOT NULL,
-  `create_date` datetime NOT NULL,
-  PRIMARY KEY (`photo_no`)
+DROP TABLE PRODUCT;
+CREATE TABLE PRODUCT (
+  `product_no` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(200) NOT NULL,
+  `product_type` varchar(50) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `business_number` int(11) NOT NULL,
+  `user_id` varchar(100) DEFAULT NULL,
+  `continent` varchar(45) NOT NULL,
+  `country` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `DELETE_FLAG` INT(2),
+  PRIMARY KEY (`product_no`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-DROP TABLE USER_RESERVATION;
-CREATE TABLE USER_RESERVATION (
-  `shop_id` varchar(45) NOT NULL,
-  `file_no` int(11) NOT NULL,
-  `file_seat` int(11) NOT NULL,
-  `shop_re_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` varchar(45) NOT NULL,
-  `shop_name` varchar(45) NOT NULL,
-  `shop_phone` varchar(45) NOT NULL
+DROP TABLE PRODUCT_REVIEW;
+CREATE TABLE PRODUCT_REVIEW (
+  `review_no` int(11) NOT NULL AUTO_INCREMENT,
+  `review_title` varchar(200) DEFAULT NULL,
+  `review_content` varchar(2000) NOT NULL,
+  `user_id` varchar(200) NOT NULL,
+  `writer_name` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `product_no` int(11) DEFAULT NULL,
+  `review_point` int(11) NOT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `DELETE_FLAG` INT(2),
+  PRIMARY KEY (`review_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+/*
+ * 1. 공지사항 NOTICE
+ * 2. 상품(투어, 티켓, 셔틀 등) PRODUCT 
+ * 3. 후기(투어, 티켓, 셔틀 등) PRODUCT_REVIEW
+ * 4. 숙박  %%예정
+ * */
+DROP TABLE PHOTO;
+CREATE TABLE PHOTO (
+  `photo_no` int(11) NOT NULL AUTO_INCREMENT,
+  `product_type` int(11) NOT NULL,
+  `board_no` int(11) NOT NULL,
+  `file_dir` varchar(200) NOT NULL,
+  `apply_user_id` varchar(50) NOT NULL,
+  `create_date` datetime NOT NULL,
+  PRIMARY KEY (`photo_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE BUSINESS;
+CREATE TABLE BUSINESS (
+  `business_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `zip_code` varchar(100) DEFAULT NULL,
+  `address1` varchar(100) DEFAULT NULL,
+  `address2` varchar(100) DEFAULT NULL,
+  `business_phone` varchar(100) DEFAULT NULL,
+  `business_number` varchar(100) DEFAULT NULL,
+  `delete_flag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`business_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+DROP TABLE MEMBER;
+CREATE TABLE MEMBER (
+  `member_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `delete_flag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`member_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+/* 대륙 - 국가 - 도시 */
 DROP TABLE MENU_CONTINENT;
 CREATE TABLE MENU_CONTINENT (
   `CONTINENT_NO` int(11) NOT NULL AUTO_INCREMENT,
@@ -89,7 +142,8 @@ CREATE TABLE MENU_COUNTRY (
   `COUNTRY` varchar(50) NOT NULL,
   `CONTINET_NO` INT(11) NOT NULL,
   `DELETE_FLAG` INT(2),
-	PRIMARY KEY (`COUNTRY_NO`)
+  PRIMARY KEY (`COUNTRY_NO`),
+  FOREIGN KEY(`CONTINET_NO`) REFERENCES MENU_CONTINENT(`CONTINENT_NO`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE MENU_CITY;
@@ -98,62 +152,6 @@ CREATE TABLE MENU_CITY (
   `CITY` varchar(50) NOT NULL,
   `COUNTRY_NO` INT(11) NOT NULL,
   `DELETE_FLAG` INT(2),
-  PRIMARY KEY (`CITY_NO`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-DROP TABLE PRODUCT_REVIEW;
-CREATE TABLE PRODUCT_REVIEW (
-  `review_no` int(11) NOT NULL AUTO_INCREMENT,
-  `review_title` varchar(200) DEFAULT NULL,
-  `review_content` varchar(2000) NOT NULL,
-  `create_date` datetime NOT NULL,
-  `user_id` varchar(200) NOT NULL,
-  `writer_name` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `business_number` int(11) DEFAULT NULL,
-  `review_point` int(11) NOT NULL,
-  PRIMARY KEY (`review_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-DROP TABLE product;
-CREATE TABLE product (
-  `product_no` int(11) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(200) NOT NULL,
-  `cost` int(11) NOT NULL,
-  `catagory` varchar(50) NOT NULL,
-  `business_number` int(11) NOT NULL,
-  `user_id` varchar(50) NOT NULL,
-  `menu_photo_file` varchar(200) NOT NULL,
-  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `continent` varchar(45) NOT NULL,
-  `country` varchar(45) NOT NULL,
-  `city` varchar(45) NOT NULL,
-  PRIMARY KEY (`product_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-DROP TABLE BUSINESS;
-CREATE TABLE BUSINESS (
-  `member_no` varchar(100) NOT NULL,
-  `company` varchar(100) DEFAULT NULL,
-  `postcode` varchar(100) DEFAULT NULL,
-  `address1` varchar(100) DEFAULT NULL,
-  `address2` varchar(100) DEFAULT NULL,
-  `business_phone` varchar(100) DEFAULT NULL,
-  `business_number` varchar(100) DEFAULT NULL,
-  `com_delete_flag` int(11) DEFAULT NULL,
-  PRIMARY KEY (`member_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-DROP TABLE MEMBER;
-CREATE TABLE MEMBER (
-  `member_no` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `phone` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `create_date` datetime DEFAULT NULL,
-  `level` varchar(100) DEFAULT NULL,
-  `delete_flag` int(11) DEFAULT NULL,
-  PRIMARY KEY (`member_no`)
+  PRIMARY KEY (`CITY_NO`),
+  FOREIGN KEY(`COUNTRY_NO`) REFERENCES MENU_COUNTRY(`COUNTRY_NO`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
