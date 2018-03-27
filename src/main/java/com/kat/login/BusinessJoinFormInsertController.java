8d783874c31b8fe4de9cb2f4f0ab7b7e32a88178
eungil lee <gee1018@naver.com>
2018-03-27 18:52:19 +0900

    [이은길] 사업자 가입 로직
    사업자가입로직 추가 작업중

--- src/main/java/com/kat/login/BusinessJoinFormInsertController.java
+++ src/main/java/com/kat/login/BusinessJoinFormInsertController.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kat.seat.model.JoinInfo;
 import com.kat.seat.service.LoginService;
 
 @Controller
@@ -13,8 +16,28 @@ public class BusinessJoinFormInsertController {
 	@Autowired
 	private LoginService loginService;
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public String businessFormSubmit() {
+		return "business_joinForm_main";
+	}
+
+	@RequestMapping("kat/join/businessjoinForm.do")
 	public ModelAndView businessjoinFormInsert(JoinInfo joinInfo) throws IllegalStateException, IOException {
-		
+System.out.println("[BusinessJoinFormInsertController] BUSINESS ACCESS");
+System.out.println("[BusinessJoinFormInsertController] joinInfo : " + joinInfo);
+
+    ModelAndView modelAndView =  new ModelAndView();
+    
+    try {
+    	loginService.joinInsertBusiness(joinInfo);
+System.out.println("[BusinessJoinFormInsertController] joinInfo : " + joinInfo);      	
+    	modelAndView.addObject("joinName", joinInfo);
+    	modelAndView.setViewName("JoinSucess_main");
+    } catch (Exception e){
+    	modelAndView.setViewName("/kat_exception/exception");
+    }
+System.out.println("===================================================");
+     return modelAndView;
 	}
 
 }
--- src/main/webapp/WEB-INF/views/business_joinForm_main.jsp
+++ src/main/webapp/WEB-INF/views/business_joinForm_main.jsp
@@ -8,24 +8,19 @@
 <title>회원가입 정보입력</title>
 
 <!-- css 가져오기 -->
-<link type="text/css" rel="stylesheet"
-	href="<%=request.getContextPath()%>/css/joinForm_css.css">
-<link type="text/css" rel="stylesheet"
-	href="<%=request.getContextPath()%>/css/home_main_css.css">
+<link type="text/css" rel="stylesheet" href="<%=request.getContextPath()%>/css/joinForm_css.css">
+<link type="text/css" rel="stylesheet" href="<%=request.getContextPath()%>/css/home_main_css.css">
 
 <!-- 스크립트 가져오기 -->
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="<%=request.getContextPath()%>/script/join_script.js"></script>
 
-
 </head>
 <body>
-	<jsp:include page="/WEB-INF/views/kat_main/katHader.jsp"
-		flush="false"></jsp:include>
+	<jsp:include page="/WEB-INF/views/kat_main/katHader.jsp" flush="false"></jsp:include>
 	<main>
 	<div class="totalView">
-		<jsp:include page="/WEB-INF/views/kat_login/joinForm.jsp"
-			flush="false"></jsp:include>
+		<jsp:include page="/WEB-INF/views/kat_login/businessjoinForm.jsp" flush="false"></jsp:include>
 	</div>
 	</main>
 </body>
