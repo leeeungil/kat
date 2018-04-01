f2633e516340b57baaff4c71e328ffa7acd8f5f3
DESKTOP-A9SUE12\tutu1 <wnsdud159!>
2018-04-01 12:05:24 +0900

    [상품 등록] 1단계
    [상품 등록] 1단계

--- src/main/java/com/kat/product/controller/AddProductController.java
+++ src/main/java/com/kat/product/controller/AddProductController.java
@@ -43,6 +43,79 @@ System.out.println("============================================================
 		return modelAndView;
 	}
 
+//	@RequestMapping(method = RequestMethod.POST)
+//	public ModelAndView addProduct(ProductInfo productInfo, HttpServletRequest request) throws IllegalStateException, IOException {
+//System.out.println("[AddProductController addProduct] PRODUCT ADD ACCESS");
+//System.out.println("[AddProductController addProduct] " + productInfo.toString());
+//		
+//		if(productInfo.getMulti_product_main()!=null){
+//			productInfo.setProduct_main_photo("null");
+//			
+//				SimpleDateFormat dayTime = new SimpleDateFormat("yyyymmdd-hhmmss");
+//				String Time = dayTime.format(new Date(System.currentTimeMillis()));
+//				String user_id = (String) request.getAttribute("user_id");
+//				productInfo.setUser_id(user_id);
+//				String imgType[] = productInfo.getMulti_product_main().getOriginalFilename().split(".");
+//				String imgName = user_id+productInfo.getProduct_type()+Time+imgType[imgType.length];
+//				String path = request.getSession().getServletContext().getRealPath("/") + "resources\\upload\\"+imgName;
+//				File file = new File(path);
+//				productInfo.getMulti_product_main().transferTo(file);
+//			
+//				String db = "/resources/upload/"+ imgName;
+//				productInfo.setProduct_main_photo(db);
+//		} else {
+//			productInfo.setProduct_main_photo("null");
+//		}
+//
+//		if(productInfo.getMulti_product_photo()!=null){
+//			productInfo.setProduct_photo1("null");
+//			productInfo.setProduct_photo2("null");
+//			productInfo.setProduct_photo3("null");
+//			productInfo.setProduct_photo4("null");
+//			productInfo.setProduct_photo5("null");
+//			productInfo.setProduct_photo6("null");
+//			productInfo.setProduct_photo7("null");
+//			productInfo.setProduct_photo8("null");
+//			productInfo.setProduct_photo9("null");
+//			productInfo.setProduct_photo10("null");
+//			
+//			for(int i =0; i<productInfo.getMulti_product_photo().size(); i++){			
+//				SimpleDateFormat dayTime = new SimpleDateFormat("yyyymmdd-hhmmss");
+//				String Time = dayTime.format(new Date(System.currentTimeMillis()));
+//				String user_id = (String) request.getAttribute("user_id");
+//				productInfo.setUser_id(user_id);
+//				String imgType[] = productInfo.getMulti_product_photo().get(i).getOriginalFilename().split(".");
+//				String imgName = user_id+productInfo.getProduct_type()+Time+imgType[imgType.length];
+//				String path = request.getSession().getServletContext().getRealPath("/") + "resources\\upload\\"+imgName;
+//				File file = new File(path);
+//				productInfo.getMulti_product_photo().get(i).transferTo(file);
+//			
+//				String db = "/resources/upload/"+ imgName;
+//				switch (i) {
+//					case 0: productInfo.setProduct_photo1(db);  break;
+//					case 1: productInfo.setProduct_photo2(db);  break;
+//					case 2: productInfo.setProduct_photo3(db);  break;
+//					case 3: productInfo.setProduct_photo4(db);  break;
+//					case 4: productInfo.setProduct_photo5(db);  break;
+//					case 5: productInfo.setProduct_photo6(db);  break;
+//					case 6: productInfo.setProduct_photo7(db);  break;
+//					case 7: productInfo.setProduct_photo8(db);  break;
+//					case 8: productInfo.setProduct_photo9(db);  break;
+//					case 9: productInfo.setProduct_photo10(db);  break;
+//				}
+//			}
+//		}else{
+//			productInfo.setProduct_photo1("null");
+//			productInfo.setProduct_photo2("null");
+//			productInfo.setProduct_photo3("null");
+//			productInfo.setProduct_photo4("null");
+//			productInfo.setProduct_photo5("null");
+//			productInfo.setProduct_photo6("null");
+//			productInfo.setProduct_photo7("null");
+//			productInfo.setProduct_photo8("null");
+//			productInfo.setProduct_photo9("null");
+//			productInfo.setProduct_photo10("null");
+//		}
 
 		ModelAndView modelAndView = new ModelAndView();
 		/*String user_id = (String) request.getSession(false).getAttribute("user_id"); // 로그인한 아이디 집어넣기
@@ -65,4 +138,8 @@ System.out.println("[AddProductController addProduct] getMultipart_product_file(
 
 		addMenuService.addProduct(menuInfo);
 System.out.println("[AddProductController addProduct] addMenuService.addMenu FINISH");*/
+//		modelAndView.setViewName("redirect:/kat/productForm/productAdd.do");
+//System.out.println("=============================================================");
+//		return modelAndView;
+//	}
 }
