806c6dfbee155f3511e5c00aaf549586549c0b26
tutu121512 <tu1215@naver.com>
2018-04-02 16:29:30 +0900

    ㅎ
    ㅎ

--- src/main/java/com/kat/seat/SeatAddController.java
+++ src/main/java/com/kat/seat/SeatAddController.java
@@ -1,20 +1,13 @@
 package com.kat.seat;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.kat.seat.model.SeatInfo;
 import com.kat.seat.service.AddSeatService;
 
 @Controller
@@ -25,60 +18,15 @@ public class SeatAddController {
 	private AddSeatService addSeatService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView getUploadForm(@RequestParam(name = "page", defaultValue = "1") int pageNumber,
-			HttpServletRequest request) throws IllegalStateException, IOException {
-
+	public ModelAndView productManageMenu(HttpServletRequest request) throws Exception {
 		System.out.println("로그 SeatList 컨트롤");
 
 		ModelAndView modelAndView = new ModelAndView();
 
 		String userid = (String) request.getSession(false).getAttribute("user_id"); // 로그인한 아이디 집어넣기
-
 		System.out.println("아이디 값 확인" + userid);
 
-		/*SeatInfoListView seatInfoListView = addSeatService.getSeatList(userid, pageNumber);*/ // 리스트 구현 // 주석처리 2018.03.28 이은길
-
-		/*System.out.println(seatInfoListView);*/ // seat 데이터 출력  주석처리 2018.03.28 이은길
-
 		modelAndView.setViewName("addSeat_main");
-		/*modelAndView.addObject("SeatListInfo", seatInfoListView);*/ //주석처리 2018.03.28 이은길 
-
 		return modelAndView;
 	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView addSeatSubmit(SeatInfo seatInfo, MultipartHttpServletRequest request)
-			throws IllegalStateException, IOException {
-
-		System.out.println("로그 SeatAdd 컨트롤");
-
-		ModelAndView modelAndView = new ModelAndView();
-
-		String userid = (String) request.getSession(false).getAttribute("user_id"); // 로그인한 아이디 집어넣기
-
-		seatInfo.setCreate_date(new Date());
-		seatInfo.setUser_id(userid);
-		seatInfo.setSeat_status("예약불가");
-		seatInfo.setReservation_id("비어있음");
-		seatInfo.setDelete_seat("사용");
-
-		long time = System.currentTimeMillis(); // 현재시간 주기
-
-		/* 업로드 폴더 시스템 물리적 경로 찾기 */
-		String uploadURI = "/uploadfile/memberphoto";
-		String dir = request.getSession().getServletContext().getRealPath(uploadURI);
-		System.out.println(dir);
-
-		// 업로드 파일의 물리적 저장
-		if (!seatInfo.getSeatPhotoFile().isEmpty()) {
-			seatInfo.getSeatPhotoFile().transferTo(new File(dir, userid + "_seat_" + time));
-			seatInfo.setFile_seatphoto(userid + "_seat_" + time);
-		}
-		addSeatService.addSeat(seatInfo); // 좌석 정보넣기
-
-		modelAndView.setViewName("redirect:/kat/seatForm/seatAdd.do");
-
-		return modelAndView;
-	}
-
 }
--- src/main/resources/sql_bak/create_table.sql
+++ src/main/resources/sql_bak/create_table.sql
@@ -190,8 +190,8 @@ DROP TABLE PRODUCT_COURSE;
 CREATE TABLE PRODUCT_COURSE (
   `product_no` int(20) NOT NULL,
   `course_no` varchar(400) NOT NULL,
-  `course` int(400) DEFAULT NULL,
-  `course_photo_dir` varchar(400) DEFAULT NULL,
+  `course` varchar(400) DEFAULT NULL,
+  `course_photo_dir` varchar(30) DEFAULT NULL,
   `delete_flag` INT(2),
   FOREIGN KEY(`product_no`) REFERENCES PRODUCT(`product_no`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
