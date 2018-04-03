24599601510c7362354e6b39680ffc7cb8b1cfc9
tutu121512 <tu1215@naver.com>
2018-04-03 10:41:41 +0900

    j
    j

--- src/main/java/com/kat/article/service/GoWithListService.java
+++ src/main/java/com/kat/article/service/GoWithListService.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kat.article.dao.Go_withDao;
 import com.kat.article.model.Go_with;
 import com.kat.article.model.Go_withInfoListView;
 
+@Service
 public class GoWithListService {
 
 	@Autowired
